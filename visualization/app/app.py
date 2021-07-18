d265fa9253016a15b1fe38027c536d965c55266a
richardcanik <canik.richard@gmail.com>
2021-07-18 23:43:02 +0200

    Point Cloud visualization

--- visualization/app/app.py
+++ visualization/app/app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = '/home/richard/loca-app/docker/context/upload'
+
+destination = None
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data={"destination": destination})
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.files.getlist("file")
+        for file in files:
+            if file:
+                file.save(os.path.join(UPLOAD_FOLDER, secure_filename(file.filename)))
+        global destination
+        destination = files[1].filename if files[1].filename else None
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- visualization/app/static/js/visualizer.js
+++ visualization/app/static/js/visualizer.js
@@ -2,9 +2,23 @@
 * Setup all visualization elements when the page is loaded.
 */
 function init() {
+    console.log(destination)
+
     // Connect to ROS.
     var ros = new ROSLIB.Ros({
       url : 'ws://localhost:9090'
+    });
+
+    ros.on('connection', function() {
+        console.log('Connection made!');
+
+        // First, we create a Param object with the name of the param.
+        var destinationPointCloud = new ROSLIB.Param({
+            ros : ros,
+            name : 'destination_point_cloud'
+        });
+        destinationPointCloud.set(destination);
+
     });
 
     // Create the main viewer.
@@ -13,26 +27,35 @@ function init() {
       width : 1500,
       height : 800,
       antialias : true,
-      background : '#f7f7f7'
+      background : '#f7f7f7',
     });
+    viewer.camera.position.x = 400
+    viewer.camera.position.y = 400
+    viewer.camera.position.z = 400
 
     // Add a grid.
-    viewer.addObject(new ROS3D.Grid());
+    var grid = new ROS3D.Grid();
+    grid.scale.x = 100;
+    grid.scale.y = 100;
+    grid.scale.z = 100;
+    viewer.addObject(grid);
 
     // Setup a client to listen to TFs.
     var tfClient = new ROSLIB.TFClient({
-      ros : ros,
-      angularThres : 0.01,
-      transThres : 0.01,
-      rate : 10.0,
-      fixedFrame : '/camera_link'
+        ros : ros,
+        angularThres : 0.01,
+        transThres : 0.01,
+        rate : 10.0,
+        fixedFrame : '/map'
     });
 
     var cloudClient = new ROS3D.PointCloud2({
         ros: ros,
         tfClient: tfClient,
         rootObject: viewer.scene,
-        topic: '/camera/depth_registered/points',
-        material: { size: 0.05, color: 0xff00ff }
+        topic: '/destination_point_cloud',
+        material: { size: 1, color: 0xff00ff },
+        max_pts: 10000000
     });
+
 }
